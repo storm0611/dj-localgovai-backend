852d51da6e78a63689b23c53931d7ffeed294dea
Stormy0611 <devstar0611@gmail.com>
2023-12-21 16:43:15 +0100

    update

--- scripts/server.sh
+++ scripts/server.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-cd ~/openssl-1.1.1u
 ./config --prefix=/usr/local
 make -j $(nproc)
 sudo make install_sw install_ssldirs
 sudo ldconfig -v
 export SSL_CERT_DIR=/etc/ssl/certs
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
-cd ~/crash_bandicoot/api.localgovai/
+cd ~/dj-localgovai-backend/
 source ./venv/bin/activate
+pip install -r req.txt
 python manage.py runserver 0.0.0.0:8000
